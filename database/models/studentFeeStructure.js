a0515c6dac38d432cff82a2267b9622be026f558
“Alok <alok.prabhakar@hotmail.com>
2025-02-25 09:23:55 +0530

    updated student fee structure model

--- database/models/studentFeeStructure.js
+++ database/models/studentFeeStructure.js
@@ -20,13 +20,17 @@ module.exports = (sequelize, DataTypes) => {
         student_id: { type: DataTypes.INTEGER, allowNull: false },
         grade_id: { type: DataTypes.INTEGER, allowNull: false },
         monthly_fee: { type: DataTypes.INTEGER, allowNull: false }, // Actual fee student pays
-        session_start: { type: DataTypes.DATE, allowNull: false }, // E.g., April 1st
-        session_end: { type: DataTypes.DATE, allowNull: false }, // E.g., March 31st next year
-        concession_note: { type: DataTypes.STRING, allowNull: true } // Optional reason for concession
+        session_start_month: { type: DataTypes.INTEGER, allowNull: false }, // 1 = Jan, 2 = Feb, etc.
+        session_end_month: { type: DataTypes.INTEGER, allowNull: false }, 
+        session_start_year: { type: DataTypes.INTEGER, allowNull: false }, // Instead of "2024-2025"
+        session_end_year: { type: DataTypes.INTEGER, allowNull: false },  
+        concession_note: { type: DataTypes.STRING, allowNull: true }, // Optional reason for concession
+        is_active: { type: DataTypes.BOOLEAN, defaultValue: true }, // To mark active fee structure
     }, {
         sequelize,
         modelName: 'StudentFeeStructure',
     });
+    
     //StudentFeeStructure.sync({alter:true})
     return StudentFeeStructure;
 };
